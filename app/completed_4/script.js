cef47288133fc3100e400b7f807342f698ec8fea
Abdella <abdellaalioncan@gmail.com>
2015-09-29 03:27:09 -0400

    part 4 complete

--- app/completed_4/script.js
+++ app/completed_4/script.js
@@ -3,5 +3,20 @@ angular.module('todoList', [])
 
 function Controller() { 
 	var vm = this;
+	vm.title = 'Todo List!';
+	vm.todoList = [];
+
+	vm.addTodo = function(){
+		var todoObject = {
+			text: vm.newTodo,
+			checked: false
+		};
+		vm.todoList.push(todoObject);
+		vm.newTodo = null;
+	}
+
+	vm.removeTodo = function(todoIndex){
+		vm.todoList.splice(todoIndex, 1);
+	}
 
 }
